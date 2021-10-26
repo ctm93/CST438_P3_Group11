92bcb1058e72c9bd0bcb9d3ea4a990fcd1018562
ctm93 <crismartinez@csumb.edu>
2021-10-26 15:07:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# CST438 P3 Group11
+
+# Description
+
+
+# Tech Stack
+
+
+# Contributors
+- Emma Larson
+- Cristina Martinez
+- Kathryn Grose
+- Jasdeep Singh Dhillon
+ 
---
+++ app/src/main/java/com/example/cst438_p3_group11/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cst438_p3_group11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
