c6663321dc2fad88e59a5a30ef6fb2a88517765c
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2021-11-04 17:51:45 +0530

    Changed Main Activity Layout Added on click listeners on the buttons Increased the Min API from 21 to 26 Changed Junit version from 4.+ to 4.13.2 Added Accent Colors in colors.xml Added login and sign up values in strings.xml Removed action bar from themes.xml Added colorAccent in themes.xml

--- app/src/main/java/com/example/cst438_p3_group11/MainActivity.java
+++ app/src/main/java/com/example/cst438_p3_group11/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.cst438_p3_group11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connectElements();
+        onClickMethods();
     }
+
+    private void onClickMethods() {
+        loginButton.setOnClickListener(v -> {
+            //TODO: Change to Login Activity
+            Toast.makeText(getApplicationContext(), "Login Button Clicked", Toast.LENGTH_SHORT).show();
+        });
+
+        signUpButton.setOnClickListener(v -> {
+            //TODO: Change to Sign Up Activity
+            Toast.makeText(getApplicationContext(), "Sign Up Button Clicked", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void connectElements() {
+        loginButton = findViewById(R.id.login_button);
+        signUpButton = findViewById(R.id.signup_button);
+    }
+
 }
