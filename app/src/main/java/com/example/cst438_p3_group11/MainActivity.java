5ad16ae0100c9e7f22d880fadfaf4d935ba34748
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2021-11-04 23:29:33 +0530

    Create intent to start login activity and add activity to manifest.

--- app/src/main/java/com/example/cst438_p3_group11/MainActivity.java
+++ app/src/main/java/com/example/cst438_p3_group11/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cst438_p3_group11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(v -> {
             //TODO: Change to Login Activity
             Toast.makeText(getApplicationContext(), "Login Button Clicked", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
         });
 
         signUpButton.setOnClickListener(v -> {
