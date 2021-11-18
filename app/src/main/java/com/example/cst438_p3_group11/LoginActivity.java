a75cfe968efa87c9a7d7fee0996ea74366761c71
Jasdeep-Singh-Dhillon <jdps2867@gmail.com>
2021-11-18 17:17:40 +0530

    Updated gitIgnore Updated Sign Up Layout Update Manifest Update LoginActivity.java Update MainActivity to launch intents

--- app/src/main/java/com/example/cst438_p3_group11/LoginActivity.java
+++ app/src/main/java/com/example/cst438_p3_group11/LoginActivity.java
@@ -1,6 +1,18 @@
 package com.example.cst438_p3_group11;
 
-public class LoginActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
     public Boolean validate(String username, String password) {
         if(username != null && password != null) {
             return true;
