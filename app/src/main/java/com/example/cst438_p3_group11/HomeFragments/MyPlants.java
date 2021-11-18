26fbfd1b8e41f4c39693ef2bddc689c46bc09c4e
Jasdeep-Singh-Dhillon <jdps2867@gmail.com>
2021-11-18 19:40:59 +0530

    Add missing home fragments

---
+++ app/src/main/java/com/example/cst438_p3_group11/HomeFragments/MyPlants.java
@@ -0,0 +1,22 @@
+package com.example.cst438_p3_group11.HomeFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.cst438_p3_group11.R;
+
+public class MyPlants extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.plants_view, container, false);
+        return v;
+    }
+}
