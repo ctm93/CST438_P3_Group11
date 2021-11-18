e38ab72afdb388bca1882aefafa629f72f38e5d6
Emma <71865150+Raisalis@users.noreply.github.com>
2021-11-17 20:22:19 -0800

    Fixed both SignUp and Login android tests to actually run. Modified validate function.

--- app/src/androidTest/java/com/example/cst438_p3_group11/SignUpActivityTest.java
+++ app/src/androidTest/java/com/example/cst438_p3_group11/SignUpActivityTest.java
@@ -1,13 +1,19 @@
 package com.example.cst438_p3_group11;
 
+import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 
 import static org.junit.Assert.*;
 
 import android.content.Context;
+import android.os.Looper;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,20 +22,21 @@ public class SignUpActivityTest {
     private SignUpActivity mSignUpActivity;
 
     @Before
-    public void createSignUpActivity() {
-        mSignUpActivity = new SignUpActivity();
+    public void createSignUpActivity() throws Throwable {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                mSignUpActivity = new SignUpActivity();
+            }
+        });
     }
 
     @Test
     public void signUpValidation_test() {
         String username1 = "user1";
         String password1 = "pass1";
-        String username2 = "user2";
-        String password2 = "pass2";
         assertTrue(mSignUpActivity.validate(username1, password1));
-        assertFalse(mSignUpActivity.validate(username1, password1));
-        assertFalse(mSignUpActivity.validate(username2, null));
-        assertFalse(mSignUpActivity.validate(null, password2));
+        assertFalse(mSignUpActivity.validate(username1, null));
+        assertFalse(mSignUpActivity.validate(null, password1));
     }
 
 }
